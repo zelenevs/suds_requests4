b93d3759c09f3422e6241aa150376bfdd9b9af75
Sergey Zelenev <sergey@cc2020.zelenev.net>
2020-08-21 05:17:32 -0500

    Modify setup

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 
 setup(
-    name='suds_requests4',
+    name='suds_requests',
     version='1.0.0',
-    description='A suds transport implemented with requests using suds-py3',
+    description='A suds transport implemented with requests using suds-community',
     long_description=open('README.rst').read(),
     author='Jason Michalski',
     author_email='jmrosal@crosal-research.com',
     py_modules=['suds_requests'],
     install_requires=['requests', 'suds-community'],
     license='MIT',
-    url='https://github.com/genusistimelord/suds_requests4',
+    url='https://github.com/zelenevs/suds_requests4',
 	python_requires='>=3.7',
 )
